fb9ffb1668a7fa9a50342973aa508f23278b6faf
Jothei <38465713+Jothei@users.noreply.github.com>
2018-04-18 10:36:59 +0200

    Type Ahead funktioniert

--- SwissTransportGUI/Form1.Designer.cs
+++ SwissTransportGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SwissTransportGUI
 {
-    partial class Form1
+    partial class Mainform
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Startposition = new System.Windows.Forms.Label();
+            this.lbl_Endstation = new System.Windows.Forms.Label();
+            this.btn_VerbindungSuchen = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.cb_start = new System.Windows.Forms.ComboBox();
+            this.cb_end = new System.Windows.Forms.ComboBox();
+            this.btn_searchststation = new System.Windows.Forms.Button();
+            this.btn_searchenstation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Startposition
+            // 
+            this.lbl_Startposition.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_Startposition.AutoSize = true;
+            this.lbl_Startposition.Location = new System.Drawing.Point(244, 24);
+            this.lbl_Startposition.Name = "lbl_Startposition";
+            this.lbl_Startposition.Size = new System.Drawing.Size(235, 17);
+            this.lbl_Startposition.TabIndex = 2;
+            this.lbl_Startposition.Text = "Bitte geben Sie Ihre Startstation ein:";
+            // 
+            // lbl_Endstation
+            // 
+            this.lbl_Endstation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_Endstation.AutoSize = true;
+            this.lbl_Endstation.Location = new System.Drawing.Point(244, 100);
+            this.lbl_Endstation.Name = "lbl_Endstation";
+            this.lbl_Endstation.Size = new System.Drawing.Size(230, 17);
+            this.lbl_Endstation.TabIndex = 3;
+            this.lbl_Endstation.Text = "Bitte geben Sie Ihre Endstation ein:";
+            // 
+            // btn_VerbindungSuchen
+            // 
+            this.btn_VerbindungSuchen.Location = new System.Drawing.Point(247, 173);
+            this.btn_VerbindungSuchen.Name = "btn_VerbindungSuchen";
+            this.btn_VerbindungSuchen.Size = new System.Drawing.Size(227, 40);
+            this.btn_VerbindungSuchen.TabIndex = 2;
+            this.btn_VerbindungSuchen.Text = "Verbindung suchen";
+            this.btn_VerbindungSuchen.UseVisualStyleBackColor = true;
+            this.btn_VerbindungSuchen.Click += new System.EventHandler(this.btn_VerbindungSuchen_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_close.Location = new System.Drawing.Point(692, 415);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(96, 23);
+            this.btn_close.TabIndex = 3;
+            this.btn_close.Text = "Schliessen";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // cb_start
+            // 
+            this.cb_start.FormattingEnabled = true;
+            this.cb_start.Location = new System.Drawing.Point(247, 60);
+            this.cb_start.Name = "cb_start";
+            this.cb_start.Size = new System.Drawing.Size(227, 24);
+            this.cb_start.TabIndex = 4;
+            // 
+            // cb_end
+            // 
+            this.cb_end.FormattingEnabled = true;
+            this.cb_end.Location = new System.Drawing.Point(247, 130);
+            this.cb_end.Name = "cb_end";
+            this.cb_end.Size = new System.Drawing.Size(232, 24);
+            this.cb_end.TabIndex = 5;
+            // 
+            // btn_searchststation
+            // 
+            this.btn_searchststation.Location = new System.Drawing.Point(496, 60);
+            this.btn_searchststation.Name = "btn_searchststation";
+            this.btn_searchststation.Size = new System.Drawing.Size(156, 24);
+            this.btn_searchststation.TabIndex = 6;
+            this.btn_searchststation.Text = "Station suchen";
+            this.btn_searchststation.UseVisualStyleBackColor = true;
+            this.btn_searchststation.Click += new System.EventHandler(this.btn_searchststation_Click);
+            // 
+            // btn_searchenstation
+            // 
+            this.btn_searchenstation.Location = new System.Drawing.Point(496, 129);
+            this.btn_searchenstation.Name = "btn_searchenstation";
+            this.btn_searchenstation.Size = new System.Drawing.Size(156, 24);
+            this.btn_searchenstation.TabIndex = 7;
+            this.btn_searchenstation.Text = "Station suchen";
+            this.btn_searchenstation.UseVisualStyleBackColor = true;
+            this.btn_searchenstation.Click += new System.EventHandler(this.btn_searchenstation_Click);
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_close;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btn_searchenstation);
+            this.Controls.Add(this.btn_searchststation);
+            this.Controls.Add(this.cb_end);
+            this.Controls.Add(this.cb_start);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_VerbindungSuchen);
+            this.Controls.Add(this.lbl_Endstation);
+            this.Controls.Add(this.lbl_Startposition);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transport App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lbl_Startposition;
+        private System.Windows.Forms.Label lbl_Endstation;
+        private System.Windows.Forms.Button btn_VerbindungSuchen;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.ComboBox cb_start;
+        private System.Windows.Forms.ComboBox cb_end;
+        private System.Windows.Forms.Button btn_searchststation;
+        private System.Windows.Forms.Button btn_searchenstation;
     }
 }
 
--- SwissTransportGUI/Form1.cs
+++ SwissTransportGUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace SwissTransportGUI
 {
-    public partial class Form1 : Form
+    public partial class Mainform : Form
     {
-        public Form1()
+        //Transport tr = new Transport();
+        public void typeahead(ComboBox comboboxname)
+        {
+            ITransport t = new Transport();
+            if (!string.IsNullOrEmpty(comboboxname.Text))
+            {
+                List<Station> stations = t.GetStations(comboboxname.Text).StationList;
+                if (stations.Count > 0)
+                {
+                    comboboxname.DataSource = null;
+                    comboboxname.DataSource = stations;
+                    comboboxname.DisplayMember = "Name";
+                }
+            }
+        }
+
+        public Mainform()
         {
             InitializeComponent();
+           
+
+            
+            
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_VerbindungSuchen_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btn_searchststation_Click(object sender, EventArgs e)
+        {
+            typeahead(cb_start);
+        }
+
+        private void btn_searchenstation_Click(object sender, EventArgs e)
+        {
+            typeahead(cb_end);
         }
     }
 }
